0ae949ca01fe90107c192189b71d7adde7a2eeb3
jdev <72287871+jdev-coder@users.noreply.github.com>
2022-04-23 17:09:44 -0700

    Update funni.js

--- funni.js
+++ funni.js
@@ -2,3 +2,5 @@ elem = document.createElement('p')
 elem.innerHTML = 'You cannot view the source of this webpage using view-source, literally.'
 
 document.body.appendChild(elem)
+
+console.log('Hello.')
